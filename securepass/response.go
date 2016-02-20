64cc43055671c21967eb07bb255aa0a108c3be38
Alessio Treglia <alessio@debian.org>
2016-02-20 18:25:43 +0000

    Classes refactoring

--- securepass/response.go
+++ securepass/response.go
@@ -1,9 +1,36 @@
 package securepass
 
+// APIResponse gives access to the response details
+type APIResponse interface {
+	ErrorCode() int
+	ErrorMessage() string
+}
+
+// ApplicationDescriptor describes the basic attributes of
+// a Securepass application
+type ApplicationDescriptor struct {
+	Label            string `json:"label"`
+	Realm            string `json:"realm"`
+	Group            string `json:"group"`
+	Write            bool   `json:"write"`
+	AllowNetworkIPv4 string `json:"allow_network_ipv4"`
+	AllowNetworkIPv6 string `json:"allow_network_ipv6"`
+	Privacy          bool   `json:"privacy"`
+}
+
 // Response is the base type for API calls responses
 type Response struct {
+	APIResponse
 	ErrorMsg string
 	RC       int
+}
+
+func (r *Response) ErrorCode() int {
+	return r.RC
+}
+
+func (r *Response) ErrorMessage() string {
+	return r.ErrorMsg
 }
 
 // PingResponse represents the /api/v1/ping call's HTTP response
@@ -13,14 +40,16 @@ type PingResponse struct {
 	Response
 }
 
+// AppAddResponse describes the expected response from the
+// /api/v1/apps/add
+type AppdAddResponse struct {
+	AppID     string `ini:"APP_ID"`
+	AppSecret string `ini:"APP_SECRET"`
+	Response
+}
+
 // AppInfoResponse encapsulates the /api/v1/apps/info call's HTTP response
 type AppInfoResponse struct {
-	Label            string
-	Realm            string
-	Group            string
-	Write            bool
-	AllowNetworkIPv4 string `json:"allow_network_ipv4"`
-	AllowNetworkIPv6 string `json:"allow_network_ipv6"`
-	Privacy          bool
+	ApplicationDescriptor
 	Response
 }
