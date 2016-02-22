3c5a0e9796bd42be1a055639c137f41e9f0e55d7
Alessio Treglia <alessio.treglia@smartodds.co.uk>
2016-02-22 13:27:43 +0000

    Unexpot SecurePass test instance

--- securepass/securepass_test.go
+++ securepass/securepass_test.go
@@ -6,10 +6,10 @@ import (
 	"time"
 )
 
-var SecurePassInst SecurePass
+var testInstance SecurePass
 
 func init() {
-	SecurePassInst = SecurePass{
+	testInstance = SecurePass{
 		AppID:     "ce64dc90d88b11e5b001de2f4665c1f2@ci.secure-pass.net",
 		AppSecret: "E2m6HawI743as61Kv0OhyPb6wAewXnwVkLLcF82rKOWe1SJ0Wd",
 		Endpoint:  DefaultRemote,
@@ -17,9 +17,9 @@ func init() {
 }
 
 func ExampleSecurePass() {
-	fmt.Println(SecurePassInst.AppID)
-	fmt.Println(SecurePassInst.AppSecret)
-	fmt.Println(SecurePassInst.Endpoint)
+	fmt.Println(testInstance.AppID)
+	fmt.Println(testInstance.AppSecret)
+	fmt.Println(testInstance.Endpoint)
 	// Output:
 	// ce64dc90d88b11e5b001de2f4665c1f2@ci.secure-pass.net
 	// E2m6HawI743as61Kv0OhyPb6wAewXnwVkLLcF82rKOWe1SJ0Wd
@@ -27,7 +27,7 @@ func ExampleSecurePass() {
 }
 
 func ExampleSecurePass_Ping() {
-	resp, err := SecurePassInst.Ping()
+	resp, err := testInstance.Ping()
 	fmt.Println(err)
 	fmt.Println(resp.IPVersion)
 	fmt.Println(resp.RC)
@@ -48,20 +48,20 @@ func ExampleSecurePass_AppAdd() {
 
 	app := fmt.Sprintf("test_fixture_%d_%d", time.Now().Unix(), rand.Int())
 	// Create a new app
-	addResponse, _ = SecurePassInst.AppAdd(&ApplicationDescriptor{
+	addResponse, _ = testInstance.AppAdd(&ApplicationDescriptor{
 		Label: app,
 	})
 	fixtureAppID = addResponse.AppID
 	fmt.Println(addResponse.ErrorCode())
 	fmt.Println(addResponse.ErrorMessage() == "")
 	// Check for its existence
-	resp, _ = SecurePassInst.AppInfo(fixtureAppID)
+	resp, _ = testInstance.AppInfo(fixtureAppID)
 	fmt.Println(resp.ErrorCode())
 	// Remove it
-	resp, _ = SecurePassInst.AppDel(fixtureAppID)
+	resp, _ = testInstance.AppDel(fixtureAppID)
 	fmt.Println(resp.ErrorCode())
 	// Check whether it does not longer exist
-	resp, _ = SecurePassInst.AppInfo(fixtureAppID)
+	resp, _ = testInstance.AppInfo(fixtureAppID)
 	fmt.Println(resp.ErrorCode())
 	// Output:
 	// 0
