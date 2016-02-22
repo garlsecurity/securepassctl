8d4f43bffd326d65dc91b3fd0e3b9689a2114841
Alessio Treglia <alessio.treglia@smartodds.co.uk>
2016-02-22 16:25:41 +0000

    Handle multi-section configuration files

--- securepass/spctl/service/service.go
+++ securepass/spctl/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"io"
 	"os"
 
 	"github.com/garlsecurity/go-securepass/securepass"
@@ -10,21 +11,54 @@ import (
 var (
 	// Service holds the global settings
 	Service *securepass.SecurePass
+	// NSSSettings holds the NSS settings
+	NSSSettings *securepass.NSSConfig
+	// SSHSettings holds the SSH settings
+	SSHSettings *securepass.SSHConfig
 )
 
 // LoadConfiguration reads configuration from files
 func LoadConfiguration(conffiles []string) error {
-	cfg, _ := ini.Load([]byte("[default]\nAPP_ID=\nAPP_SECRET=\n"))
+	cfg, _ := ini.Load([]byte("[default]\nAPP_ID=\nAPP_SECRET=\n[nss]\n\n[ssh]\n"))
 	for _, filename := range conffiles {
 		if fp, err := os.Open(filename); err == nil {
 			fp.Close()
 			cfg.Append(filename)
 		}
 	}
-	section, _ := cfg.GetSection("default")
+	defaultSection, _ := cfg.GetSection("default")
+	nssSection, _ := cfg.GetSection("nss")
+	sshSection, _ := cfg.GetSection("ssh")
 	Service = &securepass.SecurePass{Endpoint: securepass.DefaultRemote}
-	if err := section.MapTo(Service); err != nil {
+	NSSSettings = &securepass.NSSConfig{
+		DefaultGid:   100,
+		DefaultHome:  "/home",
+		DefaultShell: "/bin/bash",
+	}
+	SSHSettings = &securepass.SSHConfig{}
+	if err := defaultSection.MapTo(Service); err != nil {
+		return err
+	}
+	if err := nssSection.MapTo(NSSSettings); err != nil {
+		return err
+	}
+	if err := sshSection.MapTo(SSHSettings); err != nil {
 		return err
 	}
 	return nil
+}
+
+// WriteConfiguration saves configuration to file
+func WriteConfiguration(writer io.Writer, appid, endpoint, appsecret, realm, root string) (int64, error) {
+	globalConfig := securepass.GlobalConfig{
+		*Service,
+		*NSSSettings,
+		*SSHSettings,
+	}
+
+	cfg := ini.Empty()
+	if err := ini.ReflectFrom(cfg, &globalConfig); err != nil {
+		return 0, err
+	}
+	return cfg.WriteTo(writer)
 }
