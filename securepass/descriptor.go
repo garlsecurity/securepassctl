de263609456d1257026f084eb81f1464a689432a
Alessio Treglia <alessio.treglia@smartodds.co.uk>
2016-02-24 20:37:04 +0000

    Types refactoring

--- securepass/descriptor.go
+++ securepass/descriptor.go
@@ -1,11 +1,5 @@
 package securepass
 
-// APIResponse gives access to the response details
-type APIResponse interface {
-	ErrorCode() int
-	ErrorMessage() string
-}
-
 // ApplicationDescriptor describes the basic attributes of
 // a Securepass application
 type ApplicationDescriptor struct {
@@ -34,50 +28,6 @@ type UserDescriptor struct {
 	Token    string
 }
 
-// Response is the base type for API calls responses
-type Response struct {
-	APIResponse
-	ErrorMsg string
-	RC       int
-}
-
-// ErrorCode returns the API call's numeric return code
-func (r *Response) ErrorCode() int {
-	return r.RC
-}
-
-// ErrorMessage returns the API call's text message
-func (r *Response) ErrorMessage() string {
-	return r.ErrorMsg
-}
-
-// PingResponse represents the /api/v1/ping call's HTTP response
-type PingResponse struct {
-	IP        string
-	IPVersion int `json:"ip_version"`
-	Response
-}
-
-// AppAddResponse describes the expected response from the
-// /api/v1/apps/add
-type AppAddResponse struct {
-	AppID     string `json:"APP_ID"`
-	AppSecret string `json:"APP_SECRET"`
-	Response
-}
-
-// AppInfoResponse encapsulates the /api/v1/apps/info call's HTTP response
-type AppInfoResponse struct {
-	ApplicationDescriptor
-	Response
-}
-
-// AppListResponse encapsulates the /api/v1/apps HTTP response
-type AppListResponse struct {
-	AppID []string `json:"APP_ID"`
-	Response
-}
-
 // LogEntry is a SecurePass application's log entry
 type LogEntry struct {
 	// SecurePass response is currently broken, this
@@ -96,30 +46,6 @@ type LogEntriesByTimestamp []LogEntry
 func (l LogEntriesByTimestamp) Less(i, j int) bool { return l[i].Timestamp < l[j].Timestamp }
 func (l LogEntriesByTimestamp) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
 func (l LogEntriesByTimestamp) Len() int           { return len(l) }
-
-// LogsResponse encapsulates SecurePass application's logs
-type LogsResponse struct {
-	Logs map[string]LogEntry
-	Response
-}
-
-// GroupMemberResponse encapsulates whether a group belogs to a member
-type GroupMemberResponse struct {
-	Member bool
-	Response
-}
-
-// UserInfoResponse encapsulates the /api/v1/users/info HTTP response
-type UserInfoResponse struct {
-	UserDescriptor
-	Response
-}
-
-// UserListResponse encapsulates the /api/v1/users HTTP response
-type UserListResponse struct {
-	Username []string
-	Response
-}
 
 // NSSConfig encapsulates the SecurePass's config section '[nss]'
 type NSSConfig struct {
