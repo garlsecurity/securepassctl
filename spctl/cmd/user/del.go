95678f77083291760ab7beab185f84c2f98f58e8
Alessio Treglia <alessio.treglia@smartodds.co.uk>
2016-02-25 17:14:48 +0000

    Add user delete command

---
+++ spctl/cmd/user/del.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/codegangsta/cli"
+	"github.com/garlsecurity/securepassctl/spctl/service"
+)
+
+func init() {
+	Command.Subcommands = append(Command.Subcommands,
+		cli.Command{
+			Name:        "del",
+			Usage:       "delete user",
+			ArgsUsage:   "USERNAME",
+			Description: "Delete a user from SecurePass.",
+			Action:      ActionDel,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "yes, y",
+					Usage: "Skip confirmation",
+				},
+			},
+		})
+}
+
+// ActionDel provides the del subcommand
+func ActionDel(c *cli.Context) {
+	if len(c.Args()) != 1 {
+		log.Fatal("error: must specify a username")
+	}
+	username := c.Args()[0]
+
+	if !c.Bool("yes") {
+		var reply string
+		fmt.Fprintf(os.Stderr, "Do you want to delete the user '%s'? [y/N] ", username)
+		fmt.Scanln(&reply)
+		reply = strings.ToLower(reply)
+		if reply != "y" && reply != "yes" {
+			os.Exit(-1)
+		}
+	}
+
+	if _, err := service.Service.UserDel(username); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	log.Printf("User deleted: '%s'", username)
+
+}
